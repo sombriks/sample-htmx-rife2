1ef9c5f025e166b105d4a57b9d99011127590b9d
Leonardo Silveira <sombriks@gmail.com>
2024-05-05 21:00:19 -0300

    closes #2

--- src/main/java/sample/htmx/elements/processor/TemplateProcessor.java
+++ src/main/java/sample/htmx/elements/processor/TemplateProcessor.java
@@ -8,11 +8,11 @@ import java.util.List;
 public class TemplateProcessor {
     public static void populateList(Template template, List<Todo> todos) {
         todos.forEach(todo -> {
-            template.appendBlock("todos", "todo");
             template.setValue("id", todo.getId());
             template.setValue("description", todo.getDescription());
             template.setValue("ifTrue", todo.getDone() ? "selected" : "");
             template.setValue("ifFalse", !todo.getDone() ? "selected" : "");
+            template.appendBlock("todos", "todo");
         });
     }
 }
